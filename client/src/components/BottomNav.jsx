1aa4aa7127a28a5613548e6ca263e1b8b8650b80
rohitdeka-1 <studyrhd@gmail.com>
2026-01-08 11:42:55 +0530

    Bottom nav design change

--- client/src/components/BottomNav.jsx
+++ client/src/components/BottomNav.jsx
@@ -26,16 +26,17 @@ const BottomNav = () => {
     };
 
     return (
-        <div className="md:hidden fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 z-50">
-            <div className="flex justify-around items-center h-16">
+        <div className="md:hidden fixed bottom-0 left-0 right-0 bg-gray-900 rounded-t-3xl shadow-2xl z-50">
+            <div className="flex justify-around items-center h-16 px-2">
                 {navItems.map((item) => {
                     const isActive = location.pathname === item.path;
                     return (
                         <Link
                             key={item.name}
                             to={item.path}
-                            className={`flex flex-col items-center justify-center flex-1 h-full transition ${isActive ? 'text-blue-600' : 'text-gray-600'
-                                }`}
+                            className={`flex flex-col items-center justify-center flex-1 h-full transition-all duration-200 ${
+                                isActive ? 'text-blue-400' : 'text-gray-400 hover:text-gray-200'
+                            }`}
                         >
                             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 {getIcon(item.icon)}
