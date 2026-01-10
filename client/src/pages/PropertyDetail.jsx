358780ad52decdd05f8adb39cf6a364772513de9
rohitdeka-1 <studyrhd@gmail.com>
2026-01-11 03:19:23 +0530

    color change

--- client/src/pages/PropertyDetail.jsx
+++ client/src/pages/PropertyDetail.jsx
@@ -257,9 +257,9 @@ const PropertyDetail = () => {
                                         </div>
                                     )}
                                     {}
-                                    <div className="px-2 py-1 sm:px-4 sm:py-2 bg-blue-50 rounded-xl">
+                                    <div className="px-2 py-1 sm:px-4 sm:py-2 bg-amber-50 rounded-xl">
                                         <div className="flex items-center space-x-1 sm:space-x-1 sm:space-x-2">
-                                            <svg className="w-4 h-4 sm:w-5 sm:h-5 text-blue-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                            <svg className="w-4 h-4 sm:w-5 sm:h-5 text-amber-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                                             </svg>
